1f2b2d24b86dda96e73fd775145cfbe6e3622fa6
thecasualdev <git@thecasual.dev>
2026-04-20 17:38:59 +1000

    First draft

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,28 @@
 <br>
 
 <div id="links">
-    <h3>Socials</h3>
+
+    <h3 id="socails">Socials</h3>
     <ul>
-        <li><a href="" target="_blank"></a></li>
-        <li><a href="" target="_blank"></a></li>
-        <li><a href="" target="_blank"></a></li>
-        <li><a href="" target="_blank"></a></li>
-        <li><a href="" target="_blank"></a></li>
+        <li><a href="https://mastodon.social/@thecasualdev" target="_blank">Mastodon</a></li>
+        <li><a href="https://www.youtube.com/@thecasualdev" target="_blank">YouTube</a></li>
+        <li><a href="https://bsky.app/profile/thecasual.dev" target="_blank">Bluesky</a></li>
+        <li><a href="https://x.com/_thecasualdev" target="_blank">Twitter</a></li>
+        <li><a href="https://www.youtube.com/watch?v=WKZrHIH5ih4" target="_blank">Discord?</a></li>
     </ul>
+
+    <br>
+
+    <h3 id="resources">Resources</h3>
+    <ul>
+        <li><a href="https://gitlab.com/dashboard/home" target="_blank">GitLab</a></li>
+        <li><a href="https://github.com/thecasualdev" target="_blank">GitHub</a></li>
+        <li><a href="https://codeberg.org/thecasualdev" target="_blank">Codeberg</a></li>
+        <li><a href="http://thecasualdev.itch.io" target="_blank">Itch.io</a></li>
+    </ul>
+
+    <br>
+
 </div>
 
+<p>The source code of this site can be found <a href="https://gitlab.com/thecasualdev/website">here</a>.</p>
